bd443a6213eb022e3d2ec2865c3abe9b19799739
pedrobando <pedrobandorivera@gmail.com>
2021-11-01 18:38:22 -0500

    Install bootstrap.

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+    render(){
+        return(
+            <h1> Our react App from a component file</h1>
+        )
+    }
+}
+
+export default App;
